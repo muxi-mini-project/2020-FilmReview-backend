64c2af94bdf5e71d31b0397fc5874f1903336774
Jacksie <1799902904@qq.com>
2020-01-31 16:17:45 +0800

    change

--- database/sql.sql
+++ database/sql.sql
@@ -23,17 +23,17 @@ create table album (
 create table album_review (
     user_id char(6) not null,
     album_id char(3) not null,
-    review_id char(10) not null
+    review_id int not null
 );
 
 create table collection (
     user_id char(6) not null,
-    review_id char(10) not null,
+    review_id int not null,
     INDEX id_index (user_id)
 );
 
 create table review_like (
-    review_id char(10) not null,
+    review_id int not null,
     user_id char(6) not null,
     INDEX id_index (review_id)
 );
@@ -41,7 +41,7 @@ create table review_like (
 create table comment_like (
     user_id char(6) not null,
     comment_id int not null,
-    review_id char(10) not null,
+    review_id int not null,
     INDEX id_index (user_id)
 );
 
@@ -49,7 +49,7 @@ create table comment (
     user_id char(6) not null,
     name varchar(30) not null,
     user_picture varchar(50) not null,
-    review_id char(10) not null,
+    review_id int not null,
     comment_id int  not null,
     content TINYTEXT not null,
     time timestamp default current_timestamp,
@@ -60,7 +60,7 @@ create table user_review (
     user_id char(6) not null,
     name varchar(30) not null,
     user_picture varchar(30) not null,
-    review_id char(10) not null,
+    review_id int not null,
     title varchar(50) not null,
     content TEXT not null,
     time timestamp default current_timestamp,
