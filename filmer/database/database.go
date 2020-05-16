8d3557a358c2fb5cef2c1e700057738e1c3d6cc3
Jacksie <1799902904@qq.com>
2020-05-16 21:56:27 +0800

    2020.4.16

--- filmer/database/database.go
+++ filmer/database/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 	"log"
 )
 
@@ -13,7 +15,20 @@ var DB *gorm.DB
 func InitDB(DB *gorm.DB) *gorm.DB {
 	log.Println("DB Connect Start")
 	//连接数据库(考虑改成全局变量)
-	DB, err := gorm.Open("mysql", "root:@/filmer?charset=utf8&parseTime=True&loc-Local")
+	config := viper.New()
+	config.AddConfigPath("$GOPATH/src/github.com/muxi-mini-project/2020-FilmReview-backend/filmer/database") //手动改路径
+	config.SetConfigName("config")
+	config.SetConfigType("yaml")
+
+	if config.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	dns := fmt.Sprintf("%s:%s@/filmer?charset=utf8&parseTime=True&loc-Local",
+		config.Get("database.user"),
+		config.Get("database.pwd"))
+
+	DB, err := gorm.Open("mysql", dns)
 	if err != nil {
 		panic(err)
 	}
